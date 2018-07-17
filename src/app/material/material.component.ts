23e618ba12c10755edcdc0ce05c9321d91b5a327
Gabrielle Limach <gabrielle.lmm@gmail.com>
2018-07-17 12:06:44 -0300

    alteracoes do formulario

--- src/app/material/material.component.ts
+++ src/app/material/material.component.ts
@@ -1,4 +1,7 @@
+import { MaterialService } from './../../services/material.service';
+import { Categoria, Material, MaterialTipo } from './../../core/model';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '../../../node_modules/@angular/forms';
 
 @Component({
   selector: 'app-material',
@@ -7,9 +10,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaterialComponent implements OnInit {
 
-  constructor() { }
+  //url dos servicos
+  materiaisUrl = 'http://localhost:8080/materiais';
+  categoriasUrl = 'http://localhost:8080/categorias';
+  materiaistipoUrl = 'http://localhost:8080/materiaistipo';
 
+  categorias:Array<Categoria> = [];
+  setores=[];
+  materiais:Array<Material> = [];
+  tipos:Array<MaterialTipo> = [];
+  material = new Material();
+  Categoria = new Categoria();
+  Tipo = new MaterialTipo();
+  
+  constructor(private materiaisservice : MaterialService) { }
+  
   ngOnInit() {
+
+    //carrega na grade lista de materiais ja cadastrados 
+    this.carregarMateriais();
+    //carrega lista de categocias no select Categoria
+    this.materiaisservice.Consultas(this.categoriasUrl ).subscribe(response => this.categorias = response);
+    //carrega lista de tipos no select Tipo
+    this.materiaisservice.Consultas(this.materiaistipoUrl).subscribe(response => this.tipos = response);
+
+   // var x = document.querySelector("#campo_teste");
+    //x.removeAttribute("text-success");
+    //console.log(x.classList.length);
+
+  }
+  
+  //adicona um novo
+  adicionar(f: NgForm){
+    this.materiaisservice.adicionar( this.material,this.materiaisUrl).subscribe(
+      response => {
+        this.carregarMateriais();
+        this.novo();        
+
+      },
+      error => console.error(error));
+    }
+
+  //retorna lista ja de materiais ja cadastrados
+  carregarMateriais(){
+    this.materiaisservice.Consultas(this.materiaisUrl).subscribe(response => this.materiais = response);
   }
 
-}
+  apagar(id){
+    this.materiaisservice.deletar(id,this.materiaisUrl).subscribe(response => {
+       //remover o item da lista
+       this.carregarMateriais();
+
+      } );
+  }
+
+  alterar(id){
+
+    this.materiaisservice.Consultas(this.materiaisUrl+"/"+id).subscribe(response => this.material = response);
+
+  }
+  
+  novo() {
+   this.material = new Material();
+
+  }
+
+  }
--- src/app/tables/tables.component.ts
+++ src/app/tables/tables.component.ts
@@ -1,4 +1,7 @@
+import { MaterialService } from './../../services/material.service';
 import { Component, OnInit } from '@angular/core';
+
+import { Material } from './../../core/model';
 
 declare interface TableData {
     headerRow: string[];
@@ -13,32 +16,22 @@ declare interface TableData {
 export class TablesComponent implements OnInit {
     public tableData1: TableData;
     public tableData2: TableData;
+    public materiais: Material[] = []
+    materiaisUrl = 'http://localhost:8080/materiais';
 
-  constructor() { }
+  constructor(private materialService: MaterialService) { }
 
   ngOnInit() {
-      this.tableData1 = {
-          headerRow: [ 'ID', 'Name', 'Country', 'City', 'Salary'],
-          dataRows: [
-              ['1', 'Dakota Rice', 'Niger', 'Oud-Turnhout', '$36,738'],
-              ['2', 'Minerva Hooper', 'Curaçao', 'Sinaai-Waas', '$23,789'],
-              ['3', 'Sage Rodriguez', 'Netherlands', 'Baileux', '$56,142'],
-              ['4', 'Philip Chaney', 'Korea, South', 'Overland Park', '$38,735'],
-              ['5', 'Doris Greene', 'Malawi', 'Feldkirchen in Kärnten', '$63,542'],
-              ['6', 'Mason Porter', 'Chile', 'Gloucester', '$78,615']
-          ]
-      };
-      this.tableData2 = {
-          headerRow: [ 'ID', 'Name',  'Salary', 'Country', 'City' ],
-          dataRows: [
-              ['1', 'Dakota Rice','$36,738', 'Niger', 'Oud-Turnhout' ],
-              ['2', 'Minerva Hooper', '$23,789', 'Curaçao', 'Sinaai-Waas'],
-              ['3', 'Sage Rodriguez', '$56,142', 'Netherlands', 'Baileux' ],
-              ['4', 'Philip Chaney', '$38,735', 'Korea, South', 'Overland Park' ],
-              ['5', 'Doris Greene', '$63,542', 'Malawi', 'Feldkirchen in Kärnten', ],
-              ['6', 'Mason Porter', '$78,615', 'Chile', 'Gloucester' ]
-          ]
-      };
+    this.carregarLista();
+  }
+
+  carregarLista() {
+      this.materialService.Consultas(this.materiaisUrl).subscribe(
+          data => {
+              this.materiais = <Array<Material>>data;
+          },
+          
+      );
   }
 
 }
