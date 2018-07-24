d1a908c717190d0980aef51fdefc20b83dd2e2ab
Gabrielle Limach <gabrielle.lmm@gmail.com>
2018-07-24 10:42:12 -0300

    implementacao do metodo listar no componente solicitacao e interpolação do recurso no formulario de solicitacao

--- src/app/cadastro/solicitacoes/solicitacoes.component.ts
+++ src/app/cadastro/solicitacoes/solicitacoes.component.ts
@@ -1,3 +1,5 @@
+import { MaterialService } from 'services/material.service';
+import { Categoria } from 'core/model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./solicitacoes.component.css']
 })
 export class SolicitacoesComponent implements OnInit {
+  public categorias : Categoria[] = [];
+  categoriaUrl = 'http://localhost:8080/categorias';
+  
 
-  constructor() { }
+  constructor(private materialservice: MaterialService) { }
 
   ngOnInit() {
+    this.carregarListas();
   }
 
+  carregarListas() {
+    this.materialservice.Consultas(this.categoriaUrl).subscribe(
+        data => {
+            this.categorias = <Array<Categoria>>data;
+        },
+        
+    );
+  }
 }
+
