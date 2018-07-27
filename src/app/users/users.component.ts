3e575e3d68c3e441eba3852f06ab5f3faa055a95
Gabrielle Limach <gabrielle.lmm@gmail.com>
2018-07-27 12:01:24 -0300

    adicao da funcionalidade de cadastro de usuario e lista de usuarios ja cadastrados no banco

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
-import { UsersService } from 'services/funcionarios.service';
+import { FuncionariosService } from 'services/funcionarios.service';
+import { NgForm } from '../../../node_modules/@angular/forms';
+import { Funcionarios, Cargo, Setor } from 'core/model';
+import { MaterialService } from 'services/material.service';
 
 
 @Component({
@@ -10,11 +12,38 @@ import { UsersService } from 'services/funcionarios.service';
 })
 export class UsersComponent implements OnInit {
 
-  constructor(private UsersService: UsersService) { }
+  funcionarios;
+  cargo;
+  setor;
+  funcionario = new Funcionarios();
+
+  constructor(private funcionariosservice: FuncionariosService, private materialservice: MaterialService) { }
 
   ngOnInit() {
-   
+
+    this.materialservice.Consultas(this.funcionariosservice.funcionariosUrl)
+    .subscribe(response => {this.funcionarios = <Array<Funcionarios>> response;});
+
+    this.materialservice.Consultas(this.materialservice.cargosUrl)
+    .subscribe(cargos => {this.cargo = <Array<Cargo>> cargos});
+
+    this.materialservice.Consultas(this.materialservice.setorUrl)
+    .subscribe(setor => {this.setor = <Array<Setor>> setor})
   }
+
+  adicionarFunc(f: NgForm){
+
+    this.funcionariosservice.adcionarFuncionario(this.funcionario,this.funcionariosservice.funcionariosUrl).subscribe(
+      response => {
+        this.materialservice.carregarMateriais();       
+
+      },
+      error => console.error(error));
+  }
+ 
+  
+
+
 
  
 
--- src/services/funcionarios.service.ts
+++ src/services/funcionarios.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Funcionarios } from 'core/model';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UsersService {
 
-  constructor() { }
+
+
+export class FuncionariosService  {
+
+  funcionariosUrl = 'http://localhost:8080/funcionarios';
+
+  constructor( private http: HttpClient ) { }
+
+  adcionarFuncionario(funcionario : Funcionarios, url): Observable<Funcionarios>{
+      const headers = new HttpHeaders().set( 'Content-Type', 'application/json' );
+      headers.append( 'Content-Type', 'application/json' );
+
+      return this.http.post<Funcionarios>(url, funcionario, {headers: headers});
+
+  }
 }
+
+
+
